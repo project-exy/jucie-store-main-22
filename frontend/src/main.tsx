86e4f773b24088cf6541a9c45f7948df8ffef62e
Your Name <you@example.com>
2024-10-21 00:30:28 +0200

    okk 123

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 import { StoreProvider } from "./utils/storeContext.tsx";
 import { Toaster } from "react-hot-toast";
 

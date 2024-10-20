02bbad0fff45d0c4528a686acdcee3da36686f37
Your Name <you@example.com>
2024-10-21 00:20:23 +0200

    ok

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { StoreProvider } from "./utils/storeContext.tsx";
 import { Toaster } from "react-hot-toast";
 
@@ -19,9 +19,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           },
         }}
       />
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </StoreProvider>
   </React.StrictMode>
 );
